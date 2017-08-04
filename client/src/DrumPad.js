57267ac927d6b2379f0402370345a07c3b2f680b
judrummer <judrummercpe@gmail.com>
2017-08-05 01:20:26 +0700

    disable right click and text selected for window10 tablet

--- client/src/DrumPad.js
+++ client/src/DrumPad.js
@@ -25,7 +25,7 @@ const buttons = [
 ]
 
 export class DrumPad extends React.PureComponent {
-  render () {
+  render() {
     return (
       <div
         onTouchStart={(e) => e.preventDefault()}
@@ -37,7 +37,7 @@ export class DrumPad extends React.PureComponent {
       </div>
     )
   }
-  renderButton (button, index) {
+  renderButton(button, index) {
     const top = (Math.floor(index / 4) * 25) + '%'
     const left = ((index % 4) * 25) + '%'
     const width = '25%'
@@ -55,24 +55,37 @@ export class DrumPad extends React.PureComponent {
       </div>
     )
   }
-  handleTrigger (note, velocity) {
+  handleTrigger(note, velocity) {
     const midiVelocity = Math.max(0, Math.min(127, Math.round(velocity * 127)))
-    MIDI.send([ 0x99, note, midiVelocity ])
-    MIDI.send([ 0x89, note, midiVelocity ])
+    MIDI.send([0x99, note, midiVelocity])
+    MIDI.send([0x89, note, midiVelocity])
   }
 }
 
 const DrumButton = class DrumButton extends React.PureComponent {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.touchId = 0
   }
-  render () {
+  render() {
     const { background, backgroundActive, name } = this.props
     return (
       <div
         onTouchStart={this.handleTouchStart}
-        style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, transform: 'translateZ(0)' }}
+        onContextMenu={this.onContextMenu}
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          transform: 'translateZ(0)',
+          "-moz-user-select": "none",
+          "-khtml-user-select": "none",
+          "-webkit-user-select": "none",
+          "user-select": "none"
+        }}
+
         ref={this.registerTouchElement}
       >
         <div style={{ position: 'absolute', top: 2, right: 2, bottom: 2, left: 2, background, transform: 'translateZ(0)' }} />
@@ -92,6 +105,11 @@ const DrumButton = class DrumButton extends React.PureComponent {
       element.style.opacity = '0'
     }
   }
+
+  onContextMenu = (e) => {
+    e.preventDefault()
+  }
+
   handleTouchStart = (e) => {
     if (!this.element) return
     const touch = e.changedTouches[0]
