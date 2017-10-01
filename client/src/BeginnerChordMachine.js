ab6f5664428e8996f4cf09870dd7bd3f5ecb876a
Thai Pangsakulyanont <dtinth@spacet.me>
2017-10-01 12:57:16 +0700

    :sparkles: Display key (major and minor)

--- client/src/BeginnerChordMachine.js
+++ client/src/BeginnerChordMachine.js
@@ -4,6 +4,21 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import styled from 'react-emotion'
 
+const noteNames = [
+  'C',
+  'Db',
+  'D',
+  'Eb',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'Ab',
+  'A',
+  'Bb',
+  'B'
+]
+
 function cancelEvent (e) {
   e.preventDefault()
   e.stopPropagation()
@@ -21,6 +36,7 @@ export class BeginnerChordMachine extends React.PureComponent {
         style={{ left: 0, top: 0, bottom: 0, right: 0, position: 'absolute' }}
       >
         <div style={{ left: '50%', top: '50%', position: 'absolute' }}>
+          <KeyIndicator store={store} />
           <Circle store={store} v={[ 0, 16, 19 ]} n={1} x={-2} y={1} text='I' fn='tonic' q='' />
           <Circle store={store} v={[ 2, 17, 21 ]} n={2} x={0} y={1} text='ii' fn='subdominant' q='m' />
           <Circle store={store} v={[ 4, 19, 23 ]} n={3} x={2} y={1} text='iii' fn='tonic' q='m' />
@@ -52,6 +68,26 @@ const CircleOverlay = styled.div`
   justify-content: center;
 `
 
+const KeyIndicatorText = styled.div`
+  position: absolute;
+  top: -220px;
+  left: -200px;
+  width: 400px;
+  text-align: center;
+  color: #8b8685;
+`
+
+const KeyIndicator = observer(function KeyIndicator (props) {
+  const { store } = props
+  const keyText = [
+    noteNames[(144 + store.transpose) % 12],
+    ' major · ',
+    noteNames[(144 + store.transpose + 9) % 12],
+    ' minor'
+  ].join('')
+  return <KeyIndicatorText><strong>Key:</strong> {keyText}</KeyIndicatorText>
+})
+
 const Circle = observer(class Circle extends React.PureComponent {
   state = { active: false }
   onTouchStart = (e) => {
@@ -67,20 +103,7 @@ const Circle = observer(class Circle extends React.PureComponent {
   render () {
     const transpose = this.props.store.transpose
     const trueNoteValue = transpose + this.props.v[0]
-    const hint = [
-      'C',
-      'Db',
-      'D',
-      'Eb',
-      'E',
-      'F',
-      'F#',
-      'G',
-      'Ab',
-      'A',
-      'Bb',
-      'B'
-    ][(144 + trueNoteValue) % 12]
+    const hint = noteNames[(144 + trueNoteValue) % 12]
     const size = 160
     const spacing = 192
     const left = this.props.x * (spacing / 2)
