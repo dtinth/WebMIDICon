7f7c1a7e7540dbfb4eb1defd2f8c49860ed83e93
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-07-04 00:43:23 +0700

    :sparkles: Keyboard mode for iPedal :hankey:

--- client/src/Pedal.js
+++ client/src/Pedal.js
@@ -2,13 +2,37 @@ import * as MIDI from './MIDI'
 
 import React from 'react'
 
-export class DrumPad extends React.PureComponent {
+export class Pedal extends React.PureComponent {
+  componentDidMount () {
+    window.addEventListener('keydown', this.handleKeyDown)
+    window.addEventListener('keyup', this.handleKeyUp)
+  }
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.handleKeyDown)
+    window.removeEventListener('keyup', this.handleKeyUp)
+  }
+  _touchPressure = 0
+  _mousePressure = 0
+  _keyPressure = 0
+  handleKeyDown = (e) => {
+    if (e.which === 32) {
+      this._keyPressure = 2
+      this.updatePressure()
+    }
+  }
+  handleKeyUp = (e) => {
+    if (e.which === 32) {
+      this._keyPressure = 0
+      this.updatePressure()
+    }
+  }
   render () {
     return (
       <div
         onTouchStart={this.handleTouch}
         onTouchMove={this.handleTouch}
         onTouchEnd={this.handleTouch}
+        onMouseDown={this.handleMouseDown}
         style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
       >
         <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)', fontSize: '20vw', fontWeight: 'bold' }}>iPedal</div>
@@ -21,6 +45,22 @@ export class DrumPad extends React.PureComponent {
   }
   handleTouch = (e) => {
     const pressure = e.touches.length
+    this._touchPressure = pressure
+    this.updatePressure()
+    e.preventDefault()
+  }
+  handleMouseDown = () => {
+    this._mousePressure = 2
+    this.updatePressure()
+    const handleMouseUp = () => {
+      this._mousePressure = 0
+      this.updatePressure()
+      window.removeEventListener('mouseup', handleMouseUp)
+    }
+    window.addEventListener('mouseup', handleMouseUp)
+  }
+  updatePressure () {
+    const pressure = this._touchPressure + this._keyPressure + this._mousePressure
     const opacity = Math.pow(pressure / 11, 1 / 3)
     if (this.div) {
       this.div.style.opacity = opacity
@@ -32,8 +72,7 @@ export class DrumPad extends React.PureComponent {
       MIDI.send([ 0xB0, 0x40, 0 ])
     }
     this._pressure = pressure
-    e.preventDefault()
   }
 }
 
-export default DrumPad
+export default Pedal
