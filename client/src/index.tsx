2b2706a7bca246028e4373f4a674a6ed8f6301ff
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-17 02:08:46 +0700

    Add relevant link to documentation

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom'
 import { App } from './core'
 
 // TODO [#20]: Add a service worker to allow this app to run offline.
+// See:
+// - https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template-typescript/src/serviceWorker.ts
+// - https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
 
 const main = (() => {
   const existing = document.getElementById('main')
