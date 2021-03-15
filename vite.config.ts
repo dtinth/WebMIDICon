6b7fad33630b7490f7db62036a31152c61ebe297
dtinth on MBP M1 <dtinth@spacet.me>
2021-03-15 21:05:39 +0700

    Add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), VitePWA()],
 })
