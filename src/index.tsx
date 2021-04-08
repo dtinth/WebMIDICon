a4586ec7a33bba06062a423ab88b5f73640b295e
dtinth on MBP M1 <dtinth@spacet.me>
2021-04-09 01:49:22 +0700

    Remove stale comment

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import ReactDOM from 'react-dom'
 import { App } from './core'
 import { featureModules } from './features'
 
-// TODO [#20]: Add a service worker to allow this app to run offline.
-// See:
-// - https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template-typescript/src/serviceWorker.ts
-// - https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
 const main = (() => {
   const existing = document.getElementById('main')
   if (existing) return existing
