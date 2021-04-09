cc24dadaa7798efa7e82ab26809cda06a0125c45
dtinth on MBP M1 <dtinth@spacet.me>
2021-04-10 05:43:22 +0700

    Add useAppConfigurationContext

--- src/core/AppConfigurationContext.tsx
+++ src/core/AppConfigurationContext.tsx
@@ -1,7 +1,17 @@
-import { createContext } from 'react'
+import { createContext, useContext } from 'react'
 import { ConfigurationSchema, ConfigurationStorage } from '../configuration'
 
-export default createContext<{
+export const AppConfigurationContext = createContext<{
   storage: ConfigurationStorage
   schema: ConfigurationSchema
 } | null>(null)
+
+export default AppConfigurationContext
+
+export function useAppConfigurationContext() {
+  const context = useContext(AppConfigurationContext)
+  if (!context) {
+    throw new Error('No context')
+  }
+  return context
+}
