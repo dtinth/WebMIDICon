470e9c550c6872b151c5b125025088976ca99417
dtinth on MBP M1 <dtinth@spacet.me>
2021-04-10 20:50:24 +0700

    Extract MIDIEmitter to new file

--- src/core/MainView.jsx
+++ src/core/MainView.jsx
@@ -2,7 +2,6 @@ import * as MIDI from './MIDI'
 
 import MainToolbar from './MainToolbar'
 import React from 'react'
-import createStore from './createStore'
 import styled from 'react-emotion'
 import { Switch, Route } from 'react-router-dom'
 import { Observer } from 'mobx-react'
@@ -11,6 +10,7 @@ import FeatureList from './FeatureList'
 import WheelController from './WheelController'
 import { AppConfigurationEditor } from './AppConfigurationEditor'
 import { AppServices } from './AppServices'
+import MIDIEmitter from './MIDIEmitter'
 
 export class MainView extends React.Component {
   constructor(props) {
@@ -278,41 +278,6 @@ const ScrollViewElement = styled('div')`
 
 function ScrollView({ children }) {
   return <ScrollViewElement data-scroll-view="1">{children}</ScrollViewElement>
-}
-
-class MIDIEmitter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.currentNotes = new Map()
-  }
-  handleNotes(props) {
-    const activeNotes = props.activeNotes
-    const currentNotes = this.currentNotes
-    for (const note of activeNotes) {
-      if (!currentNotes.has(note)) {
-        const midiNote = note + props.transpose + props.octave * 12
-        const velocity = props.velocity
-        MIDI.send([0x90, midiNote, velocity])
-        currentNotes.set(note, { midiNote, velocity })
-      }
-    }
-    for (const note of currentNotes.keys()) {
-      if (!activeNotes.has(note)) {
-        const data = currentNotes.get(note)
-        MIDI.send([0x80, data.midiNote, data.velocity])
-        currentNotes.delete(note)
-      }
-    }
-  }
-  componentDidMount() {
-    this.handleNotes(this.props)
-  }
-  componentWillReceiveProps(nextProps) {
-    this.handleNotes(nextProps)
-  }
-  render() {
-    return null
-  }
 }
 
 function NewWindowRequired() {
