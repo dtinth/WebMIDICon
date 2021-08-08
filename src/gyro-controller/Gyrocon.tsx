17d3b0e828f7f866f4e339d6262ce7463b8a8885
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-09 01:55:57 +0700

    Allow using mouse as pitch bend for gyrocon

--- src/gyro-controller/Gyrocon.tsx
+++ src/gyro-controller/Gyrocon.tsx
@@ -1,14 +1,38 @@
 import { MIDI } from '../core'
 
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { tw } from 'twind'
+
+const clampRoundPitch = (x: number) =>
+  Math.max(0, Math.min(0x3fff, Math.round(x)))
 
 export function Gyrocon() {
   const [text, setText] = useState('...')
   const [pitch, setPitch] = useState(0x2000)
+  const pitchRef = useRef(pitch)
+  const divRef = useRef<HTMLDivElement>(null)
+  const [down, setDown] = useState(false)
+  const downRef = useRef(down)
   useEffect(() => {
     MIDI.send([0xe0, pitch & 0x7f, pitch >> 7])
+    pitchRef.current = pitch
   }, [pitch])
+  useEffect(() => {
+    downRef.current = down
+    if (!down) {
+      const interval = setInterval(() => {
+        if (document.pointerLockElement) {
+          setPitch((p) => {
+            const oldOffset = p - 0x2000
+            const newOffset = oldOffset * 0.9
+            const newPitch = clampRoundPitch(newOffset + 0x2000)
+            return newPitch
+          })
+        }
+      })
+      return () => clearInterval(interval)
+    }
+  }, [down])
   useEffect(() => {
     MIDI.send([0xb0, 101, 0])
     MIDI.send([0xb0, 100, 0])
@@ -18,7 +42,6 @@ export function Gyrocon() {
       const beta = e.beta || 0
       const threshold = 2.5
       const range = 10
-      // setText([e.alpha, e.beta, e.gamma].map((x) => x?.toFixed(2)).join(' '))
       const x =
         beta > threshold
           ? beta - threshold
@@ -29,20 +52,6 @@ export function Gyrocon() {
         0x2000 + Math.round(Math.max(-1, Math.min(1, x / -range)) * 0x1fff)
       setText(`${beta.toFixed(2)}`)
       setPitch(pitch)
-
-      // var gamma = e.rotationRate.gamma
-      // if (gamma > 10) {
-      //   runtime.setButton(runtime.ccw)
-      //   hp = 1
-      // } else if (gamma < -10) {
-      //   runtime.setButton(runtime.cw)
-      //   hp = 1
-      // } else if (Math.abs(gamma) < 3 && hp < 0.9) {
-      //   runtime.setButton(null)
-      // }
-      // var now = Date.now()
-      // hp *= Math.exp((now - lastTime) * -1e-3)
-      // lastTime = now
     }
     window.addEventListener('deviceorientation', listener)
     return () => {
@@ -51,11 +60,29 @@ export function Gyrocon() {
   }, [])
   return useMemo(() => {
     const x = ((pitch - 8192) / 8192) * -50 + 50
+    const onMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+      setDown(true)
+    }
+    const onMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (document.pointerLockElement && downRef.current) {
+        setPitch((p) => {
+          return clampRoundPitch(p + e.movementY * -100)
+        })
+      }
+    }
+    const onMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
+      setDown(false)
+    }
     return (
       <div className={tw`absolute inset-0 overflow-hidden`}>
         <div
           className={tw`absolute inset-0`}
           style={{ transform: `translate3d(0, ${x}%, 0)` }}
+          ref={divRef}
+          onMouseDown={onMouseDown}
+          onMouseMove={onMouseMove}
+          onMouseUp={onMouseUp}
+          onDoubleClick={() => divRef.current?.requestPointerLock()}
         >
           <div
             className={tw`flex items-center justify-center inset-x-0 top-0 h-[64px] bg-#090807`}
