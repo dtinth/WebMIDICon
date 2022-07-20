ab2d30ed0efd8bcfb2d3ac94586743f9824e73be
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-21 01:34:04 +0700

    Make iPedal absorb touch

--- src/touch-pedal/Pedal.jsx
+++ src/touch-pedal/Pedal.jsx
@@ -1,42 +1,47 @@
 import { MIDI } from '../core'
 
 import React from 'react'
+import { TouchAbsorber } from '../core/TouchAbsorber'
 
 export class Pedal extends React.PureComponent {
   render() {
     return (
-      <div
-        onTouchStart={this.handleTouch}
-        onTouchMove={this.handleTouch}
-        onTouchEnd={this.handleTouch}
-        style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
-      >
+      <TouchAbsorber>
         <div
-          style={{
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%,-50%)',
-            fontSize: '20vw',
-            fontWeight: 'bold',
-          }}
+          onTouchStart={this.handleTouch}
+          onTouchMove={this.handleTouch}
+          onTouchEnd={this.handleTouch}
+          style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
         >
-          iPedal
+          <div
+            style={{
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%,-50%)',
+              fontSize: '20vw',
+              fontWeight: 'bold',
+              fontFamily:
+                '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+            }}
+          >
+            iPedal
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+              background: 'white',
+            }}
+            ref={(div) => {
+              this.div = div
+            }}
+          />
         </div>
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            background: 'white',
-          }}
-          ref={(div) => {
-            this.div = div
-          }}
-        />
-      </div>
+      </TouchAbsorber>
     )
   }
   handleTouch = (e) => {
@@ -45,13 +50,14 @@ export class Pedal extends React.PureComponent {
     if (this.div) {
       this.div.style.opacity = opacity
     }
-    if (this._pressure <= 1 && pressure > 1) {
+    if (!this._on && pressure >= 1) {
       MIDI.send([0xb0, 0x40, 127])
+      this._on = true
     }
-    if (this._pressure > 1 && pressure <= 1) {
+    if (this._on && pressure < 1) {
       MIDI.send([0xb0, 0x40, 0])
+      this._on = false
     }
-    this._pressure = pressure
     e.preventDefault()
   }
 }
