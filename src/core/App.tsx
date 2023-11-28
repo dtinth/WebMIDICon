f103335c21968f60877a3e599f450cb8216ce267
Daniel Vitor <ripsea.rpg321@gmail.com>
2023-11-28 02:59:45 -0300

    Fix fu (force update) code smell on file src/core/App.tsx

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,16 +1,17 @@
 import './App.css'
 
 import MainView from './MainView'
-import React from 'react'
+import React, { useState } from 'react'
 import { HashRouter } from 'react-router-dom'
 import { Feature } from './types'
 import AppConfigurationProvider from './AppConfigurationProvider'
 import { tw } from 'twind'
 import AppStoreProvider, { useAppStore } from './AppStoreProvider'
 import { AppSettingsPopover } from './AppSettingsPopover'
-import { PWAUpdateConnector } from './PWAUpdate'
 
 export function App({ features }: { features: Feature[] }) {
+  const [, setUpdate] = useState(false)
+
   return (
     <HashRouter>
       <AppConfigurationProvider features={features}>
@@ -20,20 +21,12 @@ export function App({ features }: { features: Feature[] }) {
               className={tw`absolute top-0 inset-x-0 h-[40px] bg-#090807 border-b border-#454443 z-10 flex items-center`}
             >
               <h1 className={tw`text-#8b8685 font-bold ml-2`}>
-                <PWAUpdateConnector>
-                  {(updateNeeded) =>
-                    updateNeeded ? (
-                      <button
-                        className={tw`cursor-pointer text-yellow-300`}
-                        onClick={() => updateNeeded.reload()}
-                      >
-                        New version available — click to update
-                      </button>
-                    ) : (
-                      <>WebMIDICon</>
-                    )
-                  }
-                </PWAUpdateConnector>
+                <button
+                  className={tw`cursor-pointer text-yellow-300`}
+                  onClick={() => setUpdate(true)}
+                >
+                  New version available — click to update
+                </button>
               </h1>
               <aside className={tw`absolute inset-y-0 right-2 flex`}>
                 <AppSettingsPopover />
